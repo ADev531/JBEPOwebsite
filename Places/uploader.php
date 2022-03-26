dd199c40a7fd4fb6f2a8f7d60290c8cab4010ccc
A Dev <82761748+sh901soft@users.noreply.github.com>
2022-03-26 21:18:03 +0900

    Update uploader.php

--- Places/uploader.php
+++ Places/uploader.php
@@ -1,6 +1,6 @@
 <?php
-  $FileName=&_FILES['fuResume']['name'];
-  $TmpName=&_FILES['fuResume']['tmp_name'];
+  $FileName=$_FILES['fuResume']['name'];
+  $TmpName=$_FILES['fuResume']['tmp_name'];
   
   move_uploaded_file($TmpName,$FileName);
   
